ec44dea71a723ad68ecf1d379440e2bed8724e2c
Oliver D. Mendoza <oliver.r.2@hotmail.com>
2017-03-26 19:13:38 -0600

    Update ConsoleInput.java

--- src/core/input/ConsoleInput.java
+++ src/core/input/ConsoleInput.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Console Input is a Concrete Input Class that read the next line in the buffer from console and lower case it.
+ * Console Input is a Concrete Strategy that read the next line in the buffer from console and lower case it.
  * Returns the lower cased line obtained.
  */
 public class ConsoleInput implements InputStrategy {
