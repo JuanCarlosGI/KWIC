3647ab77d9e1e7e195c28e555ab4ab07d49a920a
Oliver D. Mendoza <oliver.r.2@hotmail.com>
2017-03-26 19:50:47 -0600

    Update ConsoleWriter.java

--- src/core/writer/ConsoleWriter.java
+++ src/core/writer/ConsoleWriter.java
@@ -2,7 +2,12 @@ package core.writer;
 
 import java.util.List;
 
+/**
+ * ConsoleWriter is a Concrete Write Strategy that prints all the shifted and sorted lines in console
+ */
+
 public class ConsoleWriter implements WriteStrategy {
+	// Prints each line in console
 	public void write(List<String> lines) {
 		for (String line : lines)
 			System.out.println(line);
