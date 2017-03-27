d98ada99235655b07c176ba6e637e6c32b8d2b8b
Oliver D. Mendoza <oliver.r.2@hotmail.com>
2017-03-26 19:39:46 -0600

    Update TreeSetSorter.java

--- src/core/sorter/TreeSetSorter.java
+++ src/core/sorter/TreeSetSorter.java
@@ -4,13 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeSet;
 
+/**
+ * TreeSetSorter is a Concrete Sorter Strategy that sorts the lines in descending order
+ */
+
 public class TreeSetSorter implements SortStrategy {
 	private TreeSet<String> set = new TreeSet<String>();
 	
+	// Adds the line to the TreeSet
 	public void handleNewLine(String line) {
 		set.add(line);
 	}
-
+	
+	// Returns the automatically sorted TreeSet
 	public List<String> sort() {
 		return new ArrayList<String>(set);
 	}
