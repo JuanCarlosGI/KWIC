acf6d16535191a3d8b8e83d10dfaf6f73bd244c6
Oliver D. Mendoza <oliver.r.2@hotmail.com>
2017-03-26 19:52:50 -0600

    Update FileWriter.java

--- src/core/writer/FileWriter.java
+++ src/core/writer/FileWriter.java
@@ -4,13 +4,19 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.List;
 
+/**
+ * FileWriter is a Concrete Write Strategy that stores all the shifted and sorted lines in a file
+ */
+
 public class FileWriter implements WriteStrategy {
 	private PrintWriter writer;
 	
+	// Constructor that instances the writer
 	public FileWriter(String fileName) throws FileNotFoundException {
 		writer = new PrintWriter(fileName);
 	}
 	
+	// Writes each line in a file
 	public void write(List<String> lines) {
 		for (String line : lines) {
 			writer.println(line);
