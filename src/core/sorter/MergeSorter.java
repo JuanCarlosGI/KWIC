fa4cdc191152ff6b9ca6cba4a46f5d048bbda264
Oliver D. Mendoza <oliver.r.2@hotmail.com>
2017-03-26 19:27:45 -0600

    Update MergeSorter.java

--- src/core/sorter/MergeSorter.java
+++ src/core/sorter/MergeSorter.java
@@ -3,13 +3,16 @@ package core.sorter;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Merge Sorter is a Concrete Sort Strategy that just merge the lines
+ */
 public class MergeSorter implements SortStrategy {
 	private List<String> lines = new LinkedList<String>();
 	
 	public void handleNewLine(String line) {
 		lines.add(line);
 	}
-
+	
 	public List<String> sort() {
 		lines.sort(null);
 		return lines;
